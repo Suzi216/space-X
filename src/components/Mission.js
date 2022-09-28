eba194ffb3f26da18037c561d78d49ca70106d09
Suzi216 <66145033+Suzi216@users.noreply.github.com>
2022-09-28 22:04:35 +0300

    Adding the mission table rendering

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-// import Book from './Book';
+// import Missiontable from './Missiontable';
 // import Bookform from './Bookform';
 import { displayMission } from '../redux/mission/mission';
 
@@ -9,12 +9,24 @@ const Mission = () => {
   const dispatch = useDispatch();
   console.log(allmission);
   useEffect(() => {
-    dispatch(displayMission());
+    if (!allmission.length) dispatch(displayMission());
   }, []);
 
   return (
     <div>
-      Hello from mission
+      <table>
+        <tr>
+          <th>Mission</th>
+          <th>Description</th>
+          <th>Status</th>
+        </tr>
+        {allmission.map((mission) => (
+          <tr key={mission.mission_id}>
+            <td>{mission.mission_name}</td>
+            <td>{mission.description}</td>
+          </tr>
+        ))}
+      </table>
     </div>
   );
 };
---
+++ src/components/Missiontable.js
@@ -0,0 +1,24 @@
+// import React from 'react';
+//
+// const Missiontable = (props) => {
+//   const {
+//     missionname, description,
+//   } = props;
+//   return (
+//     <table>
+//       <tr>
+//         <th>Mission</th>
+//         <th>Description</th>
+//         <th>Status</th>
+//       </tr>
+//       <tr>
+//         <td>{missionname}</td>
+//       </tr>
+//       <tr>
+//         <td>{description}</td>
+//       </tr>
+//     </table>
+//   );
+// };
+//
+// export default Missiontable;
