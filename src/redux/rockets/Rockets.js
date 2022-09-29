ed618e624698f379e5bc9203e196efb2f475125d
Aseni <alusinejalloh800@gmail.com>
2022-09-29 14:07:15 +0300

    Update `Redux Rockets file by Adding actions for Booking`

--- src/redux/rockets/Rockets.js
+++ src/redux/rockets/Rockets.js
@@ -1,4 +1,5 @@
 const ROCKETS_FETCH = 'ROCKETS_FETCH';
+const ROCKETS_RESERVE = 'ROCKETS_RESERVE';
 const URL = 'https://api.spacexdata.com/v3/rockets';
 
 const structured = (data) => {
@@ -22,11 +23,25 @@ export const rocketsFetch = async (dispatch) => {
   dispatch({ type: ROCKETS_FETCH, payload: structured(data) });
 };
 
+export const rocketsReserve = (id) => ({
+  type: ROCKETS_RESERVE,
+  id,
+});
+
 const initstate = [];
 const reducerRockets = (state = initstate, action) => {
+  let finalState;
   switch (action.type) {
     case ROCKETS_FETCH:
       return action.payload;
+    case ROCKETS_RESERVE:
+      finalState = state.map((rocket) => {
+        if (rocket.id === action.id) {
+          return { ...rocket, reserved: !rocket.reserved };
+        }
+        return rocket;
+      });
+      return finalState;
     default:
       return state;
   }
